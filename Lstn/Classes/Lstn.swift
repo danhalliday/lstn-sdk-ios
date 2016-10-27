2ab38b724a5ebc3e613c7df662c5e9b77e258256
Dan Halliday <dan@danhalliday.com>
2016-10-27 10:23:37 +0100

    Start Obj-C support

--- Lstn/Classes/Lstn.swift
+++ Lstn/Classes/Lstn.swift
@@ -1,12 +1,14 @@
 import Foundation
 
-public class Lstn {
+@objc public class Lstn: NSObject {
 
     public static let shared = Lstn()
 
     public let player = Player()
 
-    private init() {}
+    private override init() {
+        super.init()
+    }
 
     // static let API = "https://api.lstn.ltd"
     static let API = "https://private-378fa2-lstn.apiary-mock.com"
